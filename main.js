b1706a38c3bbdd729612869c34bab7ff18124db2
Fazliddindev2007 <afazliddin615@gmail.com>
2024-02-22 16:53:05 +0500

    git pood

---
+++ main.js
@@ -0,0 +1,9 @@
+const listOfCardElements = document.querySelectorAll('.card');
+const cardContainer = document.querySelector('.card-container');
+
+listOfCardElements.forEach((cardElement, index) => {
+  cardElement.addEventListener('click', () => {
+    const scrollLeft = index * listOfCardElements[0].offsetWidth;
+    cardContainer.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+  });
+});
